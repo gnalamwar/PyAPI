a533b1d0214e970fcc8d2983705be1d762233142
Gajendra Nalamwar <gaju.nalamwar@gmail.com>
2021-02-01 18:08:11 +0000

    Added 5 list py programs.

---
+++ listrev01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def main():
+    myemptylist = []
+
+
+    ## add to our list with a list method
+    ## The extend method will add every item to the list
+
+    myemptylist.extend('192.168.102.55')
+
+    ### display list
+    print(myemptylist)
+
+if __name__ == "__main__":
+    main()
